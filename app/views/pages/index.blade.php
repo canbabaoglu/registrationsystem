1ccb967040acb1e9eb2ff8cdba9411af95110fdb
Can Babaoglu <babaoglu.can@gmail.com>
2014-12-03 16:59:40 +0000

    Copy changes to home

--- app/views/pages/index.blade.php
+++ app/views/pages/index.blade.php
@@ -10,11 +10,11 @@
 
 		@if (Auth::check()) 
 			<h2>Hi {{ Auth::user()->firstname }},</h2>
-			<p>Welcome to my awesome app that doesn't do anything</p>
+			<p>Welcome to my awesome app that doesn't do anything.</p>
 		@else
 
 			<h1>My Registration System</h1>
-			<p>This is the registration system I've built with Laravel</p>
+			<p>This is the registration system I've built with Laravel.  I've pushed on GitHub.  Now, thanks to Forge, whenever I push to GitHub the site is being Autodeployed. </p>
 			<p>
 				<a class="btn btn-primary btn-lg" href="/register" role="button">Register now!</a> 
 				<a class="btn btn-link" href="/login" role="button">Already have an account? Login.</a>
