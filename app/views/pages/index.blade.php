2db3803906929d2fb860d118dbbc2f317dda3fc1
Can Babaoglu <babaoglu.can@gmail.com>
2014-12-08 12:29:47 +0000

    Made a note about this being the dev branch

--- app/views/pages/index.blade.php
+++ app/views/pages/index.blade.php
@@ -11,14 +11,18 @@
 		@if (Auth::check()) 
 			<h2>Hi {{ Auth::user()->firstname }},</h2>
 			<p>Welcome to my awesome app that doesn't do anything.</p>
+			<p>This is the dev branch.</p>
 		@else
 
 			<h1>My Registration System</h1>
 			<p>This is the registration system I've built with Laravel. Now, thanks to Forge, whenever I push to GitHub, the site is being auto-deployed. </p>
+			<p>This is the dev branch.</p>
 			<p>
 				<a class="btn btn-primary btn-lg" href="/register" role="button">Register now!</a> 
 				<a class="btn btn-link" href="/login" role="button">Already have an account? Login.</a>
 			</p>
+
+
 
 		@endif
 	</div>
