d2f0fa4e82698e14a8e8493e24c4643e087ad238
Can Babaoglu <babaoglu.can@gmail.com>
2014-12-08 14:58:01 +0000

    Changing development text on landing page

--- app/views/pages/index.blade.php
+++ app/views/pages/index.blade.php
@@ -11,12 +11,12 @@
 		@if (Auth::check()) 
 			<h2>Hi {{ Auth::user()->firstname }},</h2>
 			<p>Welcome to my awesome app that doesn't do anything.</p>
-			<h2>This is the dev branch.</h2>
+			<h2>THIS IS THE DEV BRANCH.</h2>
 		@else
 
 			<h1>My Registration System</h1>
 			<p>This is the registration system I've built with Laravel. Now, thanks to Forge, whenever I push to GitHub, the site is being auto-deployed. </p>
-			<h2>This is the dev branch.</h2>
+			<h2>THIS IS THE DEV BRANCH.</h2>
 			<p>
 				<a class="btn btn-primary btn-lg" href="/register" role="button">Register now!</a> 
 				<a class="btn btn-link" href="/login" role="button">Already have an account? Login.</a>
